ab76841f458f9092a41efc4e535a06b341180530
lamk77 <chenyinfeng50@gmail.com>
2023-03-20 12:30:35 +0800

    Added code, optimization file structure

---
+++ Raspberry_Pi/OpenCV/QR_Code/QR_Code_PYTHON.py
@@ -0,0 +1,12 @@
+import cv2
+# import numpy as np
+from pyzbar.pyzbar import decode
+
+# 加载图像
+img = cv2.imread('qrcode.png', 0)
+
+# 解码二维码
+decoded = decode(img)
+
+# 打印结果
+print(decoded[0].data.decode('utf-8'))
