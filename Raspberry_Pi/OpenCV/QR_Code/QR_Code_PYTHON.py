96a95f5757da636ae47e56d07e34ea37bc29861a
lamk77 <chenyinfeng50@gmail.com>
2023-03-23 19:10:43 +0800

    UPDATE

--- Raspberry_Pi/OpenCV/QR_Code/QR_Code_PYTHON.py
+++ Raspberry_Pi/OpenCV/QR_Code/QR_Code_PYTHON.py
@@ -1,11 +1,29 @@
 import cv2
-# import numpy as np
+import numpy as np
 from pyzbar.pyzbar import decode
 
 def decode(file_path) -> None:
-    img = cv2.imread(filename=file_path, flags=0)
-    decoded = decode(img)
-    print(decoded[0].data.decode('utf-8'))
+    img = cv2.imread(filename=file_path, flags=0)   # 加载图像
+    try:
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        sharpened = cv2.filter2D(img, -1, kernel)   # 解码二维码
+        decoded = decode(sharpened)
+        decoded = decode(img)
+        print(decoded[0].data.decode('utf-8'))
+        print(1)
+    # except Exception as e:
+    #     qrCodeDetector = cv2.QRCodeDetector()
+    #     data, bbox, straight_qrcode = qrCodeDetector.detectAndDecode(img)
+    #     print(2)
+    #     print(data)
+    except:
+        import zxing
+        reader = zxing.BarCodeReader()
+        barcode = reader.decode(file_path)
+        print(3)
+        print(barcode.parsed)
+    finally:
+        print('识别失败')
 
 if __name__ == '__main__':
-    decode('test.png')
+    decode("D:/Git Code/Car/test/test.jpg")
--- test/test_QR_Code.py
+++ test/test_QR_Code.py
@@ -1,12 +1,16 @@
 import cv2
-# import numpy as np
+import numpy as np
 from pyzbar.pyzbar import decode
 
 # 加载图像
-img = cv2.imread('test.png', 0)
-
+img = cv2.imread('test.jpg', 0)
+kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+sharpened = cv2.filter2D(img, -1, kernel)
 # 解码二维码
-decoded = decode(img)
+decoded = decode(sharpened)
+qrCodeDetector = cv2.QRCodeDetector()
+data, bbox, straight_qrcode = qrCodeDetector.detectAndDecode(img)
 
+print(data)
 # 打印结果
 print(decoded[0].data.decode('utf-8'))
