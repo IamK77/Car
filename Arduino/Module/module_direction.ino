d36db99043f97711ee8668634e7f0a0e1324cf3a
lamk77 <chenyinfeng50@gmail.com>
2023-03-30 19:33:13 +0800

    fix bug

--- Arduino/Module/module_direction.ino
+++ Arduino/Module/module_direction.ino
@@ -13,7 +13,6 @@
  *        FA使用dirc 0  FB 1  BA 2  BB 3
  *TODO: 整合特殊方向的速度数组
  */
-
 #define FAIN1 24   //左上电机a1111
 #define FAIN2 22
 #define FPWMA 2
@@ -33,144 +32,61 @@
 #define BPWMB 7
 #define Voltage A0 //使用模拟引脚
 
+#define FORWARD {1, 0}
+#define BACKWARD {0, 1}
+
 int FPwmA,FPwmB,BPwmA,BPwmB;
 double V;
+// {左前 右前 右后 左后}
+// 定义方向数组
+int forward[4][2] {FORWARD, FORWARD, FORWARD, FORWARD}; // 前进
+int backward[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 后退
+int left[4][2] {FORWARD, BACKWARD, FORWARD, BACKWARD}; // 左转
+int right[4][2] {BACKWARD, FORWARD, BACKWARD, FORWARD}; // 右转
+int forward_left[4][2] {FORWARD, FORWARD, FORWARD, FORWARD}; // 左前方
+int forward_right[4][2] {FORWARD, FORWARD, FORWARD, BACKWARD}; // 右前方
+int backward_left[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 左后方
+int backward_right[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 右后方
+int speed[4] = {50, 50, 50, 50};
+int speed_A[4] = {255, 0, 255, 0};    //for forward_left and backward_right
+int speed_B[4] = {0, 255, 0, 255};    //for forward_right and backward_left
 
-void setup(){
- 
-Serial.begin(9600);
- //TB6612电机驱动模块控制信号初始化
-  pinMode(FAIN1,OUTPUT);//控制左上电机（A）的方向（PAIN1,PAIN2)
-  pinMode(FAIN2,OUTPUT);
-  pinMode(FPWMA,OUTPUT);//A电机PWM
-  pinMode(FBIN1,OUTPUT);//控制右上电机（B）的方向（PBIN1,PBIN2)
-  pinMode(FBIN2,OUTPUT);
-  pinMode(FPWMB,OUTPUT);//B电机的PWM
-  pinMode(BAIN1,OUTPUT);//控制左下电机（C）的方向（BPIN1,BPIN2)
-  pinMode(BAIN2,OUTPUT);
-  pinMode(BPWMA,OUTPUT);//C电机的PWM
-  pinMode(BBIN1,OUTPUT);//控制右下电机(D)的方向（BBIN1,BBIN2)
-  pinMode(BBIN2,OUTPUT);
-  pinMode(BPWMB,OUTPUT);//D电机的PWM
-  
-  //初始化tb6612电机的驱动模块
-  digitalWrite(FAIN1, 1);
-  digitalWrite(FAIN2, 0);
-  digitalWrite(FBIN1, 1);
-  digitalWrite(FBIN2, 0);
-  digitalWrite(BAIN1, 1);
-  digitalWrite(BAIN2, 0);
-  digitalWrite(BBIN1, 1);
-  digitalWrite(BBIN2, 0);
-  digitalWrite(FSTBY, 1);
-  digitalWrite(BSTBY, 1);
-  analogWrite(FPWMA, 0);
-  analogWrite(FPWMB, 0);
-  analogWrite(BPWMA, 0);
-  analogWrite(BPWMB, 0);
-}
-void SetPWM(int motor, int pwm){
-   if(motor==1&&pwm>=0)//motor=1代表控制电机A，pwm>=0则(AIN1, AIN2)=(1, 0)为正转
-  {
-    digitalWrite(FAIN1, 1);
-    digitalWrite(FAIN2, 0);
-    analogWrite(FPWMA, pwm);
-  }
-   else if(motor==1&&pwm<0)//motor=1代表控制电机A，pwm<0则(AIN1, AIN2)=(0, 1)为反转
-  {
-    digitalWrite(FAIN1, 0);
-    digitalWrite(FAIN2, 1);
-    analogWrite(FPWMA, -pwm);
-  }
-  else if(motor==2&&pwm>=0)//motor=2代表控制电机B，pwm>=0则(BIN1, BIN2)=(0, 1)为正转
-  {
-    digitalWrite(FBIN1, 0);
-    digitalWrite(FBIN2, 1);
-    analogWrite(FPWMB, pwm);
-  }
-  else if(motor==2&&pwm<0)//motor=2代表控制电机B，pwm<0则(BIN1, BIN2)=(1, 0)为反转
-  {
-    digitalWrite(FBIN1, 1);
-    digitalWrite(FBIN2, 0);
-    analogWrite(FPWMB, -pwm);
-  }
-   else if(motor==3&&pwm>=0)//motor=3代表控制电机c，pwm>=0则(BAIN1, BAIN2)=(0, 1)为正转
-  {
-    digitalWrite(BAIN1, 0);
-    digitalWrite(BAIN2, 1);
-    analogWrite(BPWMA, pwm);
-  }
-  else if(motor==3&&pwm<0)//motor=代表控制电机C，pwm<0则(BAIN1, BAIN2)=(1, 0)为反转
-  {
-    digitalWrite(BAIN1, 1);
-    digitalWrite(BAIN2, 0);
-    analogWrite(BPWMA, -pwm);
-  }
-  
-   else if(motor==4&&pwm>=0)//motor=4代表控制电机D，pwm>=0则(BBIN1, BBIN2)=(0, 1)为正转
-  {
-    digitalWrite(BBIN1, 0);
-    digitalWrite(BBIN2, 1);
-    analogWrite(BPWMB, pwm);
-  }
-  else if(motor==4&&pwm<0)//motor=4代表控制电机D，pwm<0则(BBIN1, BBIN2)=(1, 0)为反转
-  {
-    digitalWrite(BBIN1, 1);
-    digitalWrite(BBIN2, 0);
-    analogWrite(BPWMB, -pwm);
+// 
+void setup() {
+  int pins[] = {BAIN1, BAIN2, BBIN1, BBIN2, BPWMA, BPWMB, BSTBY, FAIN1, FAIN2, FBIN1, FBIN2, FPWMA, FPWMB, FSTBY};
+  int pins_size = sizeof(pins) / sizeof(pins[0]);
+  for (int i = 0; i < pins_size; i++) {
+    pinMode(pins[i], OUTPUT); //设置引脚为输出
   }
 }
-void up(){
-  SetPWM(1,-100);  //右前
-  SetPWM(2,100);   //左前
-  SetPWM(3,-100);   //左后
-  SetPWM(4,100);   //右后
+
+void move(int duration, int speed[4], int direction[4][2]) {
+  digitalWrite(BAIN1, direction[2][0]);
+  digitalWrite(BAIN2, direction[2][1]);
+  digitalWrite(BBIN1, direction[3][0]);
+  digitalWrite(BBIN2, direction[3][1]);
+  analogWrite(BPWMA, speed[2]); //设置引脚BPWMA的PWM输出为speed[2]
+  analogWrite(BPWMB, speed[3]); //设置引脚BPWMB的PWM输出为speed[3]
+  digitalWrite(BSTBY, 1); //设置引脚BSTBY为高电平
+  digitalWrite(FAIN1, direction[0][0]);
+  digitalWrite(FAIN2, direction[0][1]);
+  digitalWrite(FBIN1, direction[1][0]);
+  digitalWrite(FBIN2, direction[1][1]);
+  analogWrite(FPWMA, speed[0]); //设置引脚FPWMA的PWM输出为speed[0]
+  analogWrite(FPWMB, speed[1]); //设置引脚FPWMB的PWM输出为speed[1]
+  digitalWrite(FSTBY, 1); //设置引脚FSTBY为高电平
+  delay(duration); //延时duration毫秒
 }
-void back(){
-  SetPWM(1,100);
-  SetPWM(2,-100);
-  SetPWM(3,100);
-  SetPWM(4,-100);
-}
-void TurnLeft(){
-  SetPWM(1,-128);
-  SetPWM(2,-128);
-  SetPWM(3,128);
-  SetPWM(4,128);
-}
-void TurnRight(){
-  SetPWM(1,128);
-  SetPWM(2,128);
-  SetPWM(3,-128);
-  SetPWM(4,-128);
-}
-void RightLine(){
-   SetPWM(1,-128);
-   SetPWM(2, 128);
-   SetPWM(3, 128);
-   SetPWM(4,-128);
-}
-void LeftLine(){
-   SetPWM(1,-128);
-   SetPWM(2,-128);
-   SetPWM(3,128);
-   SetPWM(4,128);
-}
-void loop() 
-{
-  // V=analogRead(Voltage); //读取模拟引脚A0模拟量
-  // Serial.print(V*0.05371);  //对模拟量转换并通过串口输出
-  // Serial.println("V");
-  up();
-  // delay(500);
-  // back();
-  // delay(500);
-  // TurnLeft();
-  // delay(500);
-  // TurnRight();
-  // delay(500);
-  // RightLine();
-  // delay(500);
-  // LeftLine();
-  // delay(500);
+
+void loop() {
+  move(1000, speed, forward); //向前移动1000毫秒，速度为255
+  // direction[0] = BACKWARD;
+  // direction[1] = BACKWARD;
+  move(100000, {50,50,50,50}, direction); //向后移动1000毫秒，速度为255
+  // direction[0] = LEFT;
+  // direction[1] = RIGHT;
+  // move(1000, speed, direction); //向左移动1000毫秒，速度为255
+  // direction[0] = RIGHT;
+  // direction[1] = LEFT;
+  // move(1000, speed, direction); //向右移动1000毫秒，速度为255
 }
