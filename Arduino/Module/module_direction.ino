f22042833140e823e56e0980985f755fa900791f
lamk77 <chenyinfeng50@gmail.com>
2023-03-30 21:11:33 +0800

    fix bug

--- Arduino/Module/module_direction.ino
+++ Arduino/Module/module_direction.ino
@@ -13,21 +13,21 @@
  *        FA使用dirc 0  FB 1  BA 2  BB 3
  *TODO: 整合特殊方向的速度数组
  */
-#define FAIN1 24   //左上电机a1111
+#define FAIN1 24   //右前电机a
 #define FAIN2 22
 #define FPWMA 2
 #define FSTBY 3
 
-#define FBIN1 26     //右上电机b
+#define FBIN1 26     //左前电机b
 #define FBIN2 28
 #define FPWMB 4 
 
-#define BAIN1 32    //左下电机c
+#define BAIN1 32    //左后电机a
 #define BAIN2 30
 #define BPWMA 5  
 #define BSTBY 6
 
-#define BBIN2 34   //D
+#define BBIN2 34   //右后b
 #define BBIN1 36
 #define BPWMB 7
 #define Voltage A0 //使用模拟引脚
@@ -37,7 +37,7 @@
 
 int FPwmA,FPwmB,BPwmA,BPwmB;
 double V;
-// {左前 右前 右后 左后}
+// {右前 左前 左后 右后}
 // 定义方向数组
 int forward[4][2] {FORWARD, FORWARD, FORWARD, FORWARD}; // 前进
 int backward[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 后退
@@ -47,7 +47,8 @@ int forward_left[4][2] {FORWARD, FORWARD, FORWARD, FORWARD}; // 左前方
 int forward_right[4][2] {FORWARD, FORWARD, FORWARD, BACKWARD}; // 右前方
 int backward_left[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 左后方
 int backward_right[4][2] {BACKWARD, BACKWARD, BACKWARD, BACKWARD}; // 右后方
-int speed[4] = {50, 50, 50, 50};
+int speed[4] = {51, 57, 56, 50};
+int speed_r[4] = {50, 50, 50, 50};
 int speed_A[4] = {255, 0, 255, 0};    //for forward_left and backward_right
 int speed_B[4] = {0, 255, 0, 255};    //for forward_right and backward_left
 
@@ -63,15 +64,15 @@ void setup() {
 void move(int duration, int speed[4], int direction[4][2]) {
   digitalWrite(BAIN1, direction[2][0]);
   digitalWrite(BAIN2, direction[2][1]);
-  digitalWrite(BBIN1, direction[3][0]);
-  digitalWrite(BBIN2, direction[3][1]);
+  digitalWrite(BBIN1, direction[3][1]);
+  digitalWrite(BBIN2, direction[3][0]);
   analogWrite(BPWMA, speed[2]); //设置引脚BPWMA的PWM输出为speed[2]
   analogWrite(BPWMB, speed[3]); //设置引脚BPWMB的PWM输出为speed[3]
   digitalWrite(BSTBY, 1); //设置引脚BSTBY为高电平
-  digitalWrite(FAIN1, direction[0][0]);
-  digitalWrite(FAIN2, direction[0][1]);
-  digitalWrite(FBIN1, direction[1][0]);
-  digitalWrite(FBIN2, direction[1][1]);
+  digitalWrite(FAIN1, direction[0][1]);
+  digitalWrite(FAIN2, direction[0][0]);
+  digitalWrite(FBIN1, direction[1][1]);
+  digitalWrite(FBIN2, direction[1][0]);
   analogWrite(FPWMA, speed[0]); //设置引脚FPWMA的PWM输出为speed[0]
   analogWrite(FPWMB, speed[1]); //设置引脚FPWMB的PWM输出为speed[1]
   digitalWrite(FSTBY, 1); //设置引脚FSTBY为高电平
@@ -79,10 +80,10 @@ void move(int duration, int speed[4], int direction[4][2]) {
 }
 
 void loop() {
-  move(1000, speed, forward); //向前移动1000毫秒，速度为255
+  move(1000000, speed_r, right); //向前移动1000毫秒，速度为255555555555555555555555555555555555555555555555555555555555555555555555+
   // direction[0] = BACKWARD;
   // direction[1] = BACKWARD;
-  move(100000, {50,50,50,50}, direction); //向后移动1000毫秒，速度为255
+  // move(100000, {50,50,50,50}, direction); //向后移动1000毫秒，速度为255
   // direction[0] = LEFT;
   // direction[1] = RIGHT;
   // move(1000, speed, direction); //向左移动1000毫秒，速度为255
